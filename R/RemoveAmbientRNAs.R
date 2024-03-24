403d41572e4f0dedb880942ecc5e82bf82e7ef2a
JarningGau <jarninggau@gmail.com>
2024-03-24 17:33:47 +0800

    refactor: update `RemoveAmbientRNAs()`

--- R/RemoveAmbientRNAs.R
+++ R/RemoveAmbientRNAs.R
@@ -1,11 +1,10 @@
 #' A wrapper of celda::decontX
 #' @param seu Seurat object
-#' @param split.by The grouping variable used to split the input object.
-#' @param cluster.name Name of cluster field in Seurat object.
-#' @param library.id Name of library field in Seurat object.
+#' @param split.by The grouping variable used to split the input object. Default: NULL.
+#' @param cluster.name Name of cluster field in Seurat object. Default: NULL.
 #' @return seurat object including a corrected counts matrix and contamination rates for each cell.
 #' @export
-RemoveAmbientRNAs <- function(seu, split.by, cluster.name = NULL, library.id = NULL) {
+RemoveAmbientRNAs <- function(seu, split.by = NULL, cluster.name = NULL) {
   if (is.null((cluster.name))) {
     stop("The clusters must be pre-defined.")
   }
@@ -13,7 +12,7 @@ RemoveAmbientRNAs <- function(seu, split.by, cluster.name = NULL, library.id = N
   if (is.null(split.by)) {
     sce <- celda::decontX(sce, z=sce[[cluster.name]])
   } else {
-    sce <- celda::decontX(sce, z=sce[[cluster.name]], batch=sce[[library.id]])
+    sce <- celda::decontX(sce, z=sce[[cluster.name]], batch=sce[[split.by]])
   }
   seu[["decontX"]] <- CreateAssayObject(counts = round(celda::decontXcounts(sce), 0))
   seu$decontX_contamination <- SingleCellExperiment::colData(sce)$decontX_contamination
